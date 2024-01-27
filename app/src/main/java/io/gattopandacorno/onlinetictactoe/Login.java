ec2183806295e5cb9d832419542d014522f39a7c
Caterina Gazzotti <gazzotti.caterina@gmail.com>
2024-01-27 12:26:59 +0100

    First implementation of online mode with firebase

--- app/src/main/java/io/gattopandacorno/onlinetictactoe/Login.java
+++ app/src/main/java/io/gattopandacorno/onlinetictactoe/Login.java
@@ -1,14 +1,21 @@
 package io.gattopandacorno.onlinetictactoe;
 
 import androidx.activity.OnBackPressedCallback;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.Objects;
 
@@ -16,7 +23,10 @@ import java.util.Objects;
 public class Login extends AppCompatActivity
 {
 
-    @SuppressLint("SetTextI18n")
+    private final String url= "https://onlinetictactoe-4e7b9-default-rtdb.europe-west1.firebasedatabase.app/";
+
+
+    @SuppressLint({"SetTextI18n", "NewApi"})
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -49,14 +59,21 @@ public class Login extends AppCompatActivity
         else
         {
             setContentView(R.layout.formonline);
-            EditText t = findViewById(R.id.player);
+            EditText t = findViewById(R.id.player), code = findViewById(R.id.code);
+            String c = code.getText().toString();
+            DatabaseReference db = FirebaseDatabase.getInstance().getReferenceFromUrl(url);
 
+            // Set click listener for when Join button is touched
             findViewById(R.id.join).setOnClickListener(v -> {
 
                 if(!t.getText().toString().isEmpty()) i.putExtra("playerName2", t.getText().toString());
                 else i.putExtra("playerName2", "PLAYER2");
 
-                //TODO: the code to control code here
+                if(!c.isEmpty() && db.child("codes").get()!=null)
+                    db.child("codes").child("players").push().setValue(t.getText().toString());
+                else
+                    Toast.makeText(this, "Enter a valid code", Toast.LENGTH_SHORT).show();
+
                 //TODO: set name for player 1
 
                 i.putExtra("host", false);
@@ -64,21 +81,31 @@ public class Login extends AppCompatActivity
 
                 startActivity(i);
                 finish();
-            });//TODO: insert logic to control the code
+            });
 
+            // Set click listener for when Create/Host button is touched
             findViewById(R.id.host).setOnClickListener(v ->{
 
+                // Set value for the player who hosts the game; if not given the default is "PLAYER1"
                 if(!t.getText().toString().isEmpty()) i.putExtra("playerName1", t.getText().toString());
                 else i.putExtra("playerName1", "PLAYER1");
 
-                //TODO: the code to generate code here
-                //TODO: set name for player 2 when enters the room
+                // Control if the code for the
+                if(!c.isEmpty() && db.child("codes").get() == null)
+                {
+                    db.child("codes").setValue(c);
+                    db.child("codes").child("players").setValue(t.getText().toString());
+                }
+                    //TODO: set name for player 2 when enters the room
+                else
+                    Toast.makeText(this, "Enter a valid code", Toast.LENGTH_SHORT).show();
+
 
                 i.putExtra("host", true);
                 i.putExtra("online", true);
                 startActivity(i);
                 finish();
-            });//TODO: insert logic to generate the code
+            });
         }
 
 
@@ -95,5 +122,14 @@ public class Login extends AppCompatActivity
 
     }
 
-
+    /**
+     * Used to control if the code given by the joining player is available online
+     * It searches in all the 'entry' of the snapshot for a value equal to the given code
+     */
+    private boolean isAvailable(DataSnapshot snap, String code)
+    {
+        for (DataSnapshot s : snap.getChildren())
+            if(Objects.requireNonNull(s.getValue()).toString().equals(code)) return true;
+        return false;
+    }
 }
